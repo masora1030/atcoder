9e404f6673e47cb321ec254a160ff4d879ed2caf
Skygood <soraemonpockt@gmail.com>
2020-01-10 21:10:04 +0900

    [AC] abc150 abc150_b

---
+++ atcoder.jp/abc150/abc150_b/Main.py
@@ -0,0 +1,10 @@
+n = int(input())
+s = input()
+ans = 0
+for i in range(n-2):
+    if s[i] == 'A':
+        if s[i+1] ==  'B':
+            if s[i+2] ==  'C':
+                ans+=1
+                
+print(ans)
