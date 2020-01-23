55b5b5eb9ca8a26687c796752f94d7c45b681acd
Skygood <soraemonpockt@gmail.com>
2020-01-23 18:07:36 +0900

    [AC] abc145 abc145_d

---
+++ atcoder.jp/abc145/abc145_d/Main.py
@@ -0,0 +1,25 @@
+def cmb(n, r, mod):
+    if ( r<0 or r>n ):
+        return 0
+    r = min(r, n-r)
+    return g1[n] * g2[r] * g2[n-r] % mod
+
+mod = 10**9+7 #出力の制限
+N = 10**6
+g1 = [1, 1] # 元テーブル
+g2 = [1, 1] #逆元テーブル
+inverse = [0, 1] #逆元テーブル計算用テーブル
+
+for i in range( 2, N + 1 ):
+    g1.append( ( g1[-1] * i ) % mod )
+    inverse.append( ( -inverse[mod % i] * (mod//i) ) % mod )
+    g2.append( (g2[-1] * inverse[-1]) % mod )
+
+
+x, y = map(int, input().split())
+ans = 0
+if ((x+y)%3 == 0):
+    X = (2*x - y)//3
+    Y = (2*y - x)//3
+    ans = cmb(X+Y,X,mod)
+print(ans)
