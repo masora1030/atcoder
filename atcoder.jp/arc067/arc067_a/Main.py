02fa6bd848852f2dc34120254e0d1543bf5a4fef
Skygood <soraemonpockt@gmail.com>
2020-05-27 16:45:11 +0900

    [AC] arc067 arc067_a

---
+++ atcoder.jp/arc067/arc067_a/Main.py
@@ -0,0 +1,46 @@
+# input()
+# int(input())
+# map(int, input().split())
+# list(map(int, input().split()))
+import math
+import sys
+import bisect
+import heapq  # 優先度付きキュー(最小値取り出し)
+from collections import deque
+inf = 10 ** 15
+mod = 10 ** 9 + 7
+
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
+def factorization(n):
+    arr = []
+    temp = n
+    if n == 1:
+        return arr
+
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
+n = int(input())
+judge = [1 for i in range(10**3)] # 素数が何個あるか
+for i in range(n,1,-1):
+    tmp = factorization(i)
+    for v,num in tmp:
+        judge[v]+=num
+ans = 1
+for num in judge[2:]:
+    ans = (ans*num)%mod
+print(ans)
