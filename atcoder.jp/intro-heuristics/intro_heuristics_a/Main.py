037b9ab3fb66e349a0fe30a80c48c7905c417b1c
Skygood <soraemonpockt@gmail.com>
2020-06-28 22:44:06 +0900

    [AC] intro-heuristics intro_heuristics_a

---
+++ atcoder.jp/intro-heuristics/intro_heuristics_a/Main.py
@@ -0,0 +1,93 @@
+# input()
+# int(input())
+# map(int, input().split())
+# list(map(int, input().split()))
+# list(map(int, list(input()))) # スペースがない数字リストを読み込み
+import math
+import fractions
+import sys
+import bisect
+import heapq  # 優先度付きキュー(最小値取り出し)
+import collections
+from collections import Counter
+from collections import deque
+import pprint
+import itertools
+
+sr = lambda: input()
+ir = lambda: int(sr())
+lr = lambda: list(map(int, sr().split()))
+
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
+
+
+def factorization(n):
+    arr = []
+    temp = n
+    if n == 1:
+        return arr
+
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp != 1:
+        arr.append([temp, 1])
+
+    if arr == []:
+        arr.append([n, 1])
+
+    return arr
+
+
+# a^n
+def power(a, n, mod):
+    x = 1
+    while n:
+        if n & 1:
+            x *= a % mod
+        n >>= 1
+        a *= a % mod
+    return x % mod
+
+
+# n*(n-1)*...*(l+1)*l
+def kaijo(n, l, mod):
+    if n == 0:
+        return 1
+    a = n
+    tmp = n - 1
+    while (tmp >= l):
+        a = a * tmp % mod
+        tmp -= 1
+    return a
+
+
+inf = 10 ** 18
+mod = 10 ** 9 + 7
+
+D = ir()
+c = lr()
+d = [0 for i in range(26)]
+s = [lr() for i in range(D)]
+
+for k in range(1,D+1):
+    now = -1
+    pre = -inf
+    for i in range(26):
+        total = s[k-1][i]
+        sumc = 0
+        for j in range(26):
+            if j != i:
+                sumc += c[j]*(k-d[j])
+        total-=sumc
+        if total > pre:
+            pre = total
+            now = i
+    print(now+1)
+    d[now] = k
