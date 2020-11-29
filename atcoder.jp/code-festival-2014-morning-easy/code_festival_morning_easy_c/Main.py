3cea6c9aeae61fbf0dfa1ccb95475eefa0a3123b
Skygood <soraemonpockt@gmail.com>
2020-11-29 16:15:49 +0900

    [AC] code-festival-2014-morning-easy code_festival_morning_easy_c

---
+++ atcoder.jp/code-festival-2014-morning-easy/code_festival_morning_easy_c/Main.py
@@ -0,0 +1,169 @@
+# input()
+# int(input())
+# map(int, input().split())
+# list(map(int, input().split()))
+# list(map(int, list(input()))) # スペースがない数字リストを読み込み
+import math
+import fractions
+import sys
+import bisect
+import heapq  # 優先度付きキュー(最小値取り出し)
+import collections
+from collections import Counter
+from collections import deque
+import pprint
+import itertools
+from functools import lru_cache
+
+sr = lambda: input()
+ir = lambda: int(sr())
+lr = lambda: list(map(int, sr().split()))
+
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
+def factorization(n):
+    arr = []
+    temp = n
+    if n == 1:
+        return arr
+
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp != 1:
+        arr.append([temp, 1])
+
+    if arr == []:
+        arr.append([n, 1])
+
+    return arr
+
+
+# a^n
+def power(a, n, mod):
+    x = 1
+    while n:
+        if n & 1:
+            x *= a % mod
+        n >>= 1
+        a *= a % mod
+    return x % mod
+
+
+# n*(n-1)*...*(l+1)*l
+def kaijo(n, l, mod):
+    if n == 0:
+        return 1
+    a = n
+    tmp = n - 1
+    while (tmp >= l):
+        a = a * tmp % mod
+        tmp -= 1
+    return a
+
+# Union Find
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
+
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+# 約数生成
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+
+    divisors.sort()
+    return divisors
+
+inf = 10 ** 18
+# mod = 10 ** 9 + 7
+mod = 998244353
+
+n,m = lr()
+s,t = lr()
+s-=1
+t-=1
+
+
+e = [[] for i in range(n)]
+for k in range(m):
+    x,y,d = lr()
+    e[x-1].append([d, y-1])
+    e[y-1].append([d, x-1])
+
+# ダイクストラで始点からの各ノードへのコストを返す (eという配列にstart:[dist, end]というエッジを格納)
+def dijkstra(s):
+    hq = [(0, s)]
+    heapq.heapify(hq)  # リストを優先度付きキューに変換
+    cost = [inf for i in range(len(e))]
+    cost[s] = 0  # 開始地点は0
+    while hq:
+        c, v = heapq.heappop(hq)
+        if c > cost[v]:  # コストが現在のコストよりも高ければスルー
+            continue
+        for d, u in e[v]:
+            tmp = d + cost[v]
+            if tmp < cost[u]:
+                cost[u] = tmp
+                heapq.heappush(hq, (tmp, u))
+    return cost
+
+start_cost = dijkstra(s)
+end_cost = dijkstra(t)
+ans = -1
+for i in range(n):
+    if start_cost[i] != inf:
+        if start_cost[i] == end_cost[i]:
+            ans = i+1
+print(ans)
