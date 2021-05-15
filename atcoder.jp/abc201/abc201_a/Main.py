814c0b3252669677dd0348bbd47ba332b7543cc1
Skygood <soraemonpockt@gmail.com>
2021-05-16 00:00:01 +0900

    [AC] abc201 abc201_a

---
+++ atcoder.jp/abc201/abc201_a/Main.py
@@ -0,0 +1,25 @@
+import sys
+
+# sys.setrecursionlimit(10**4)
+# from sys import stdin
+# readline = stdin.readline
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+import bisect
+
+from sys import stdout
+
+inf=10**18
+mod=10**9+7
+# mod = 998244353
+# len(bin(n)) - bin(n).rfind("1") - 1
+
+if __name__=='__main__':
+    a = lr()
+    a.sort()
+    if a[1]-a[0] == a[2]-a[1]:
+        print('Yes')
+    else:
+        print("No")
