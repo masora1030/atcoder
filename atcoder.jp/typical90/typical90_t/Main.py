0f4b617d2908a8b4ab47dde8412cdbac3aa0cfa6
Skygood <soraemonpockt@gmail.com>
2021-07-03 01:44:37 +0900

    [AC] typical90 typical90_t

---
+++ atcoder.jp/typical90/typical90_t/Main.py
@@ -0,0 +1,23 @@
+import sys
+import bisect
+from collections import deque
+import itertools
+import math
+
+# sys.setrecursionlimit(10**4)
+from sys import stdin
+# readline = stdin.readline
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    a,b,c = lr()
+    if a < c**b:
+        print("Yes")
+    else:
+        print("No")
