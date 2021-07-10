a1348ee1dedb4e3399734ef5b316cc64512758bd
Skygood <soraemonpockt@gmail.com>
2021-07-10 21:01:00 +0900

    [AC] abc209 abc209_a

---
+++ atcoder.jp/abc209/abc209_a/Main.py
@@ -0,0 +1,20 @@
+import sys
+import bisect
+from collections import deque
+import itertools
+import math
+
+# sys.setrecursionlimit(10**4)
+from sys import stdin
+# readline = stdin.readline
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    a,b = lr()
+    print(max(b-a+1, 0))
