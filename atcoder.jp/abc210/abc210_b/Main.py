17877637b2f37461b1b445d36439055404f44f18
Skygood <soraemonpockt@gmail.com>
2021-07-17 21:05:33 +0900

    [AC] abc210 abc210_b

---
+++ atcoder.jp/abc210/abc210_b/Main.py
@@ -0,0 +1,27 @@
+import sys
+import bisect
+from collections import deque
+import itertools
+import math
+
+# sys.setrecursionlimit(10**4)
+from sys import stdin
+# readline = stdin.readline
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+mod=10**9+7
+# mod = 998244353
+
+if __name__=='__main__':
+    n=ir()
+    s=sr()
+    for i,c in enumerate(list(s)):
+        if c=='1':
+            if i%2==0:
+                print('Takahashi')
+            else:
+                print('Aoki')
+            sys.exit()
