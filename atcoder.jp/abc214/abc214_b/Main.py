ca60c9472ee9118b9bf9ffebdb2872b82f79c324
Skygood <soraemonpockt@gmail.com>
2021-08-15 20:42:48 +0900

    [AC] abc214 abc214_b

---
+++ atcoder.jp/abc214/abc214_b/Main.py
@@ -0,0 +1,17 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    s,t = lr()
+    ans = 0
+    for a in range(s+1):
+        for b in range(s+1):
+            for c in range(s+1):
+                if a+b+c <= s and a*b*c <= t:
+                    ans+=1
+    print(ans)
