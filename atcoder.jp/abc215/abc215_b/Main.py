e06228c052f6ff633a549fb71641bbdbeda82ebe
Skygood <soraemonpockt@gmail.com>
2021-08-21 21:12:44 +0900

    [AC] abc215 abc215_b

---
+++ atcoder.jp/abc215/abc215_b/Main.py
@@ -0,0 +1,16 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    n=ir()
+    tmp = 1
+    ans = 0
+    while n >= tmp:
+        tmp*=2
+        ans+=1
+    print(ans-1)
