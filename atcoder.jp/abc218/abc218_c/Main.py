7897ec234c518f25cb53be250554737de67dae8b
Skygood <soraemonpockt@gmail.com>
2021-09-12 01:32:43 +0900

    [AC] abc218 abc218_c

---
+++ atcoder.jp/abc218/abc218_c/Main.py
@@ -0,0 +1,351 @@
+# input()
+# int(input())
+# map(int, input().split())
+# list(map(int, input().split()))
+# list(map(int, list(input()))) # スペースがない数字リストを読み込み
+import math
+import fractions
+import sys
+import bisect
+import heapq  # 優先度付きキュー(最小値取り出し)
+import collections
+from collections import Counter
+from collections import deque
+import pprint
+import itertools
+from functools import lru_cache
+import cmath
+
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
+def factorization(n):
+    arr = []
+    temp = n
+    if n == 1:
+        return arr
+
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp != 1:
+        arr.append([temp, 1])
+
+    if arr == []:
+        arr.append([n, 1])
+
+    return arr
+
+
+# a^n
+def power(a, n, mod):
+    x = 1
+    while n:
+        if n & 1:
+            x *= a % mod
+        n >>= 1
+        a *= a % mod
+    return x % mod
+
+
+# n*(n-1)*...*(l+1)*l
+def kaijo(n, l, mod):
+    if n == 0:
+        return 1
+    a = n
+    tmp = n - 1
+    while (tmp >= l):
+        a = a * tmp % mod
+        tmp -= 1
+    return a
+
+# Union Find
+class UnionFind():
+    def __init__(self, n):
+        self.n=n
+        self.parents=[-1]*n
+
+    def find(self, x):
+        if self.parents[x]<0:
+            return x
+        else:
+            self.parents[x]=self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x=self.find(x)
+        y=self.find(y)
+
+        if x==y:
+            return
+
+        if self.parents[x]>self.parents[y]:
+            x, y=y, x
+
+        self.parents[x]+=self.parents[y]
+        self.parents[y]=x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x)==self.find(y)
+
+    def members(self, x):
+        root=self.find(x)
+        return [i for i in range(self.n) if self.find(i)==root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x<0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
+
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+    def all_family_count(self):
+        return [-min(0, x) for i, x in enumerate(self.parents)]
+
+    def memberlist(self):
+        self.L=[set() for _ in range(self.n)]
+        for i in range(self.n):
+            if self.parents[i]<0:
+                self.L[i].add(i)
+            else:
+                self.L[self.find(i)].add(i)
+        return self.L
+
+# 約数生成
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+
+    divisors.sort()
+    return divisors
+
+
+# segment tree
+class SegmentTree:
+    # 初期化処理
+    # f : SegmentTreeにのせるモノイド
+    # default : fに対する単位元
+    def __init__(self, size, f=lambda x, y: x+y, default=0):
+        self.size=2**(size-1).bit_length()  # 簡単のため要素数Nを2冪にする
+        self.default=default
+        self.dat=[default]*(self.size*2)  # 要素を単位元で初期化
+        self.f=f
+
+    def update(self, i, x):
+        i+=self.size
+        self.dat[i]=x
+        while i>0:
+            i>>=1
+            self.dat[i]=self.f(self.dat[i*2], self.dat[i*2+1])
+
+    def query(self, l, r):
+        l+=self.size
+        r+=self.size
+        lres, rres=self.default, self.default
+        while l<r:
+            if l & 1:
+                lres=self.f(lres, self.dat[l])
+                l+=1
+
+            if r & 1:
+                r-=1
+                rres=self.f(self.dat[r], rres)  # モノイドでは可換律は保証されていないので演算の方向に注意
+            l>>=1
+            r>>=1
+        res=self.f(lres, rres)
+        return res
+
+def div2num(x):
+    return len(bin(x))-bin(x).rfind("1")-1
+
+
+class Bit:
+    def __init__(self, n):
+        self.size = n
+        self.tree = [0]*(n+1)
+
+    def __iter__(self):
+        psum = 0
+        for i in range(self.size):
+            csum = self.sum(i+1)
+            yield csum - psum
+            psum = csum
+        raise StopIteration()
+
+    def __str__(self):  # O(nlogn)
+        return str(list(self))
+
+    def sum(self, i):
+        # [0, i) の要素の総和を返す
+        if not (0 <= i <= self.size): raise ValueError("error!")
+        s = 0
+        while i>0:
+            s += self.tree[i]
+            i -= i & -i
+        return s
+
+    def add(self, i, x):
+        if not (0 <= i < self.size): raise ValueError("error!")
+        i += 1
+        while i <= self.size:
+            self.tree[i] += x
+            i += i & -i
+
+    def __getitem__(self, key):
+        if not (0 <= key < self.size): raise IndexError("error!")
+        return self.sum(key+1) - self.sum(key)
+
+    def __setitem__(self, key, value):
+        # 足し算と引き算にはaddを使うべき
+        if not (0 <= key < self.size): raise IndexError("error!")
+        self.add(key, value - self[key])
+
+
+class Bit:
+    def __init__(self, n):
+        self.size=n
+        self.tree=[0]*(n+1)
+        self.depth=n.bit_length()
+
+    def sum(self, i):
+        s=0
+        while i>0:
+            s+=self.tree[i]
+            i-=i & -i
+        return s
+
+    def add(self, i, x):
+        while i<=self.size:
+            self.tree[i]+=x
+            i+=i & -i
+
+    def lower_bound(self, x):
+        """ 累積和がx以上になる最小のindexと、その直前までの累積和 """
+        sum_=0
+        pos=0
+        for i in range(self.depth, -1, -1):
+            k=pos+(1 << i)
+            if k<=self.size and sum_+self.tree[k]<x:
+                sum_+=self.tree[k]
+                pos+=1 << i
+        return pos+1, sum_
+
+
+import sys
+import bisect
+from collections import deque
+import itertools
+import math
+import heapq
+import random
+
+# import sys
+# sys.setrecursionlimit(10**6)
+from sys import stdin
+# readline = stdin.readline
+# sr=lambda: readline()
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    n=ir()
+    s = [sr() for i in range(n)]
+    t = [sr() for i in range(n)]
+    x_min = inf
+    x_max = 0
+    y_min = inf
+    y_max = 0
+    for i in range(n):
+        for j in range(n):
+            if s[i][j] == '#':
+                y_max = max(i, y_max)
+                y_min = min(y_min, i)
+                x_max = max(j, x_max)
+                x_min = min(x_min, j)
+
+    x_min_pre=inf
+    x_max_pre=0
+    y_min_pre=inf
+    y_max_pre=0
+    for i in range(n):
+        for j in range(n):
+            if t[i][j]=='#':
+                y_max_pre=max(i, y_max_pre)
+                y_min_pre=min(y_min_pre, i)
+                x_max_pre=max(j, x_max_pre)
+                x_min_pre=min(x_min_pre, j)
+
+    flg=True
+    for k in range(4):
+        flg=True
+        if k==0 and x_max_pre-x_min_pre==x_max-x_min and y_max_pre-y_min_pre==y_max-y_min:
+            for i in range(y_max - y_min + 1):
+                for j in range(x_max - x_min + 1):
+                    tmp = t[y_min_pre+i][x_min_pre+j]
+                    if s[y_min+i][x_min+j]!=tmp:
+                        flg=False
+                        break
+                if not flg:
+                    break
+            if flg:
+                print("Yes")
+                sys.exit()
+        elif k==1 and y_max_pre-y_min_pre==x_max-x_min and x_max_pre-x_min_pre==y_max-y_min:
+            for i in range(y_max-y_min+1):
+                for j in range(x_max-x_min+1):
+                    tmp = t[y_min_pre+j][x_max_pre-i]
+                    if s[y_min+i][x_min+j]!=tmp:
+                        flg=False
+                        break
+                if not flg:
+                    break
+            if flg:
+                print("Yes")
+                sys.exit()
+        elif k==2 and x_max_pre-x_min_pre==x_max-x_min and y_max_pre-y_min_pre==y_max-y_min:
+            for i in range(y_max-y_min+1):
+                for j in range(x_max-x_min+1):
+                    tmp = t[y_max_pre-i][x_max_pre-j]
+                    if s[y_min+i][x_min+j]!=tmp:
+                        flg=False
+                        break
+                if not flg:
+                    break
+            if flg:
+                print("Yes")
+                sys.exit()
+        elif k==3 and y_max_pre-y_min_pre==x_max-x_min and x_max_pre-x_min_pre==y_max-y_min:
+            for i in range(y_max-y_min+1):
+                for j in range(x_max-x_min+1):
+                    tmp = t[y_max_pre-j][x_min_pre+i]
+                    if s[y_min+i][x_min+j]!=tmp:
+                        flg=False
+                        break
+                if not flg:
+                    break
+            if flg:
+                print("Yes")
+                sys.exit()
+    print("No")
+
