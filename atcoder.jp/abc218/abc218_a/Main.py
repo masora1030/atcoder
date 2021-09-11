73b55115797fd9293b43ebaf9a851966019db2ea
Skygood <soraemonpockt@gmail.com>
2021-09-12 01:33:10 +0900

    [AC] abc218 abc218_a

---
+++ atcoder.jp/abc218/abc218_a/Main.py
@@ -0,0 +1,16 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    n=ir()
+    s=sr()
+    n-=1
+    if s[n] == 'o':
+        print("Yes")
+    else:
+        print("No")
