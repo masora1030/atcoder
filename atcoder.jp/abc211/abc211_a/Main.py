dd6de035070da0ffbb2a2ca0cdb0009d1e26d474
Skygood <soraemonpockt@gmail.com>
2021-09-18 18:38:53 +0900

    [AC] abc211 abc211_a

---
+++ atcoder.jp/abc211/abc211_a/Main.py
@@ -0,0 +1,12 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+# mod=10**9+7
+mod = 998244353
+
+if __name__=='__main__':
+    a,b = lr()
+    c = (a-b)/3 + b
+    print(c)
