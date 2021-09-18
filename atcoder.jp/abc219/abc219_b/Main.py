86b821754631707cdfb6c830fe80dc27b8d63601
Skygood <soraemonpockt@gmail.com>
2021-09-18 21:04:27 +0900

    [AC] abc219 abc219_b

---
+++ atcoder.jp/abc219/abc219_b/Main.py
@@ -0,0 +1,16 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+inf=10**18
+mod=10**9+7
+# mod = 998244353
+
+if __name__=='__main__':
+    s = [sr() for i in range(3)]
+    t = sr()
+    ans = []
+    for c in t:
+        ind = int(c)-1
+        ans.append(s[ind])
+    print(''.join(ans))
