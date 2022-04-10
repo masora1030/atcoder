c1c630ba7bd6ea79acf91da8d9802c6f3d3dd230
Skygood <soraemonpockt@gmail.com>
2022-04-10 21:01:29 +0900

    [AC] abc247 abc247_a

---
+++ atcoder.jp/abc247/abc247_a/Main.py
@@ -0,0 +1,17 @@
+sr=lambda: input()
+ir=lambda: int(sr())
+lr=lambda: list(map(int, sr().split()))
+
+# dr=lambda: float(sr())
+# dlr=lambda: list(map(float, sr().split()))
+
+inf=10**18
+# mod = 10**9+7
+mod=998244353
+
+def main():
+    s = sr()
+    print(''.join(['0', s[:3]]))
+
+if __name__ == '__main__':
+    main()
